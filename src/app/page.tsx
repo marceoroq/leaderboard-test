ea5baf86fb2924a02a6ae7308d5cd56a934aa047
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-03-10 11:00:39 -0300

    refactor(Pages): remove main and move rank page to ranks url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-mono">
-      <main className="flex flex-col row-start-2 items-center sm:items-start">Root Page</main>
+    <div className="flex flex-col row-start-2 items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-mono sm:items-start">
+      Root Page
     </div>
   );
 }
