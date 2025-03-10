3c72710a1eb19e5a6779712f75306a2b52e71c91
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-03-10 10:59:58 -0300

    feat(Layout): add globalnav to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GlobalNav from "@/components/GlobalNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} flex flex-col antialiased h-screen`}>
+        <GlobalNav />
+        <main className="flex-grow overflow-hidden">{children}</main>
+      </body>
     </html>
   );
 }
