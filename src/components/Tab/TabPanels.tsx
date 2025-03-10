6f5c697cd4070b453391ec099e44564ae8ac8dee
Marcelo Oroquieta <oroquieta.m@gmail.com>
2025-03-10 13:40:45 -0300

    style(TabPanels): adjust some styles

--- src/components/Tab/TabPanels.tsx
+++ src/components/Tab/TabPanels.tsx
@@ -4,5 +4,5 @@ interface TabPanelsProps {
 }
 
 export const TabPanels: React.FC<TabPanelsProps> = ({ children, className = "" }) => {
-  return <div className={`mt-4 overflow-hidden ${className}`}>{children}</div>;
+  return <div className={`w-full mt-4 overflow-hidden ${className}`}>{children}</div>;
 };
